0e2a04519feb1a18157300edeb65faa7822d2e9f
fpmBelkov <62466605+fpmBelkov@users.noreply.github.com>
2020-11-14 19:29:10 +0300

    Add files via upload

---
+++ src/main/java/com/famcs/SQLRequests.java
@@ -0,0 +1,13 @@
+package com.famcs;
+
+public class SQLRequests {
+        public static final String SHORTNAME = "shortname";
+        public static final String EMPLOYEE_NUMBER = "employee_number";
+        public static final String ACTIVITY_TYPE = "activity_type";
+        public static final String TABLE_NAME = "company_table";
+        public static final String SELECT = "select";
+        public static final String FROM = "from";
+        public static final String WHERE = "where";
+        public static final String AND = "and";
+        public static final String BETWEEN = "between";
+    }
